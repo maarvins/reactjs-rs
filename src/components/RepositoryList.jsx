b1f3ca5299a18ce44e4b1c4fb60b59ae23c9ec48
Marcus Sena <maarvins.19@gmail.com>
2021-12-04 12:47:59 -0300

    feat: create RepositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositórios</h1>{" "}
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar Repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
