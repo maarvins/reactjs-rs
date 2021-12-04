af2947bc64bd152918e62629aa582d75f1785ef8
Marcus Sena <maarvins.19@gmail.com>
2021-12-04 14:07:02 -0300

    feat: add counter logic to understand the state

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+export function Counter() {
+  function increment() {
+    return console.log("incrementing")
+  }
+
+  return (
+    <div>
+      <h2>0</h2>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+    </div>
+  )
+}
