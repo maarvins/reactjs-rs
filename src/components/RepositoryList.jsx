7c9818d6d2abe4b677cb40e7679ff7e5ff27fc9d
Marcus Sena <maarvins.19@gmail.com>
2021-12-04 13:51:40 -0300

    feat: using PROPS

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,17 @@
+import {RepositoryItem} from "./RepositoryItem"
+
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/"
+}
+
 export function RepositoryList() {
   return (
     <section>
-      <h1>Lista de repositórios</h1>{" "}
+      <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-          <a href="">Acessar Repositório</a>
-        </li>
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
