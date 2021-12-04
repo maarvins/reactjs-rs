b0572e3cbba5392b377d05f61307c787aef72cba
Marcus Sena <maarvins.19@gmail.com>
2021-12-04 15:13:54 -0300

    chore: receiving data from api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,5 @@
+import {useState, useEffect} from "react"
+
 import {RepositoryItem} from "./RepositoryItem"
 
 import "../styles/repositories.scss"
@@ -9,6 +11,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/maarvins/repos") //buscar os valores do OBJETO dentro da api
+      .then((response) => response.json()) //quando eu obter os valores da api, vou transformar em um JSON
+      .then((data) => console.log(data)) //depois da conversão para json, terei os dados do repositório
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
